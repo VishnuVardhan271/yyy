43a504aa9ccc8e7d29f8d7cb939793a4ffd3f977
VishnuVardhan271 <135198974+VishnuVardhan271@users.noreply.github.com>
2023-07-09 12:44:25 +0530

    Update TaskController.java

--- controller/TaskController.java
+++ controller/TaskController.java
@@ -35,117 +35,175 @@ public class TaskController {
 	// method for view the tasks for user
 	@RequestMapping(value = "/tasksbyid", method = RequestMethod.GET)
 	public String viewTasksForUser(Model model, HttpSession session) {
-		// Check if the user is logged in
-		User user = (User) session.getAttribute("user");
-		if (user == null) {
-			// User is not logged in, redirect to the login page
-			return "redirect:/login";
+		try {
+			// Check if the user is logged in
+			User user = (User) session.getAttribute("user");
+			if (user == null) {
+				// User is not logged in, redirect to the login page
+				return "redirect:/login";
+			}
+
+			// User is logged in, fetch the tasks
+			List<TaskDto> tasks = taskService.getTasksByUserId(user.getUserId());
+
+			// Add the tasks to the model
+			model.addAttribute("tasks", tasks);
+			logger.info("Tasks loaded by user");
+			// Return the view name
+			String userTask = "User";
+			model.addAttribute("Task", userTask);
+			return "Taskslist";
+		} catch (Exception e) {
+			logger.error("Error occurred while loading tasks for user", e);
+			// Add error message to the model
+			model.addAttribute("error", "An error occurred while loading tasks.");
+			return "errorPage";
 		}
-
-		// User is logged in, fetch the tasks
-		List<TaskDto> tasks = taskService.getTasksByUserId(user.getUserId());
-
-		// Add the tasks to the model
-		model.addAttribute("tasks", tasks);
-		logger.info("tasks  loaded by user");
-		// Return the view name
-		String userTask = "User";
-		model.addAttribute("Task", userTask);
-		return "Taskslist";
 	}
 
 	// method for get all the tasks
 	@RequestMapping(value = "/tasks", method = RequestMethod.GET)
 	public String viewAllTasks(Model model) {
+		try {
+			List<TaskDto> tasks = taskService.getAllTasks();
 
-		List<TaskDto> tasks = taskService.getAllTasks();
-
-		// Add the tasks to the model
-		model.addAttribute("tasks", tasks);
-		logger.info("Viewing all tasks");
-		String userTask = "Task";
-		model.addAttribute("Task", userTask);
-		// Return the view name
-		return "Taskslist";
+			// Add the tasks to the model
+			model.addAttribute("tasks", tasks);
+			logger.info("Viewing all tasks");
+			String userTask = "Task";
+			model.addAttribute("Task", userTask);
+			// Return the view name
+			return "Taskslist";
+		} catch (Exception e) {
+			logger.error("Error occurred while loading all tasks", e);
+			// Add error message to the model
+			model.addAttribute("error", "An error occurred while loading tasks.");
+			return "errorPage";
+		}
 	}
 
 	// method for get the task details by id
 	@RequestMapping(value = "/taskdetailsbyid", method = RequestMethod.GET)
 	public String getAllTasks(Model model) {
+		try {
+			List<TaskDto> tasks = taskService.getAllTasks();
 
-		List<TaskDto> tasks = taskService.getAllTasks();
-
-		model.addAttribute("tasks", tasks);
-		logger.info("Viewing task details by ID");
-		return "tasksdetailsbyid";
+			model.addAttribute("tasks", tasks);
+			logger.info("Viewing task details by ID");
+			return "tasksdetailsbyid";
+		} catch (Exception e) {
+			logger.error("Error occurred while loading task details", e);
+			// Add error message to the model
+			model.addAttribute("error", "An error occurred while loading task details.");
+			return "errorPage";
+		}
 	}
 
 	// method for update status
 	@RequestMapping(value = "/updateTaskStatus", method = RequestMethod.GET)
 	public String updateTaskStatus(@RequestParam("taskId") int taskId, Model model) {
-		// Retrieve the existing task from the database using the task ID
-		Task task = taskService.getTaskById(taskId);
-		model.addAttribute("task", task);
-		logger.info("Updating task status");
+		try {
+			// Retrieve the existing task from the database using the task ID
+			Task task = taskService.getTaskById(taskId);
+			model.addAttribute("task", task);
+			logger.info("Updating task status");
 
-		return "taskStatusUpdate";
+			return "taskStatusUpdate";
+		} catch (Exception e) {
+			logger.error("Error occurred while updating task status", e);
+			// Add error message to the model
+			model.addAttribute("error", "An error occurred while updating task status.");
+			return "errorPage";
+		}
 	}
 
-	// method for update sucess
+	// method for update success
 	@RequestMapping(value = "/updateSuccess", method = RequestMethod.POST)
 	public String updateTaskStatusSuccess(@RequestParam("taskId") int taskId) {
+		try {
+			// Retrieve the existing task from the database using the task ID
+			Boolean task = taskService.updateStatus(taskId);
 
-		// Retrieve the existing task from the database using the task ID
-		Boolean task = taskService.updateStatus(taskId);
+			// Update the task status
 
-		// Update the task status
-
-		// Redirect to the task list page
-		return "redirect:/tasks";
+			// Redirect to the task list page
+			return "redirect:/tasks";
+		} catch (Exception e) {
+			logger.error("Error occurred while updating task status", e);
+			// Add error message to the model
+			model.addAttribute("error", "An error occurred while updating task status.");
+			return "errorPage";
+		}
 	}
 
 	@RequestMapping(value = "/Taskfilter", method = RequestMethod.POST)
 	@ResponseBody
 	public String filterTasks(@Validated TFilterCriteria filterCriteria) {
-		List<TaskDto> filteredTasks = taskService.TfilterTasks(filterCriteria);
-		System.out.println(filteredTasks);
+		try {
+			List<TaskDto> filteredTasks = taskService.TfilterTasks(filterCriteria);
+			System.out.println(filteredTasks);
 
-		Gson gson = new Gson();
-		String json = gson.toJson(filteredTasks);
-		logger.info("Filtered tasks");
+			Gson gson = new Gson();
+			String json = gson.toJson(filteredTasks);
+			logger.info("Filtered tasks");
 
-		return json;
+			return json;
+		} catch (Exception e) {
+			logger.error("Error occurred while filtering tasks", e);
+			// Handle the exception accordingly (e.g., return an error JSON)
+			return "{\"error\": \"An error occurred while filtering tasks.\"}";
+		}
 	}
 
 	// method for get individual tasks
 	@RequestMapping(value = "/Indvtasks", method = RequestMethod.GET)
 	public String viewIndvtasks(@RequestParam("projId") Integer projId, Model model) {
-		// Fetch tasks based on the provided project ID
-		List<Task> tasks = taskService.getTasksByProjectId(projId);
+		try {
+			// Fetch tasks based on the provided project ID
+			List<Task> tasks = taskService.getTasksByProjectId(projId);
 
-		// Add the tasks to the model
-		model.addAttribute("tasks", tasks);
-		logger.info("Individual tasks loaded");
+			// Add the tasks to the model
+			model.addAttribute("tasks", tasks);
+			logger.info("Individual tasks loaded");
 
-		return "Indvtasks"; // Replace with actual view name
+			return "Indvtasks"; // Replace with actual view name
+		} catch (Exception e) {
+			logger.error("Error occurred while loading individual tasks", e);
+			// Add error message to the model
+			model.addAttribute("error", "An error occurred while loading individual tasks.");
+			return "errorPage";
+		}
 	}
 
 	// method for set task status
 	@RequestMapping(value = "/setTaskStatus", method = RequestMethod.GET)
 	public String setTaskStatus(@RequestParam int taskId, Model model, HttpSession session) {
-		// Fetch the task based on the provided task ID
-		Task task = taskService.getTaskById(taskId);
-		model.addAttribute("task", task);
-		logger.info("Setting task status");
-		return "taskStatusUpdate";
+		try {
+			// Fetch the task based on the provided task ID
+			Task task = taskService.getTaskById(taskId);
+			model.addAttribute("task", task);
+			logger.info("Setting task status");
+			return "taskStatusUpdate";
+		} catch (Exception e) {
+			logger.error("Error occurred while setting task status", e);
+			// Add error message to the model
+			model.addAttribute("error", "An error occurred while setting task status.");
+			return "errorPage";
+		}
 	}
 
-	// method for set task details
+	// methodfor set task details
 	@RequestMapping(value = "/setTaskDetails", method = RequestMethod.GET)
 	public String setTaskUpdateFormModel(@Validated TaskUpdateFormModel taskUpdateFormModel, Model model) {
-		// Update the task status based on the provided form model
-		taskService.updateTaskStatus(taskUpdateFormModel);
-		return "redirect:activity";
+		try {
+			// Update the task status based on the provided form model
+			taskService.updateTaskStatus(taskUpdateFormModel);
+			return "redirect:activity";
+		} catch (Exception e) {
+			logger.error("Error occurred while setting task details", e);
+			// Add error message to the model
+			model.addAttribute("error", "An error occurred while setting task details.");
+			return "errorPage";
+		}
 	}
-
 }
